5f8348707830301b7ae05e2f2bc7b2439f0eed11
zahar1143123 <zaharrubanik2007@gmail.com>
2023-04-01 22:50:59 +0300

    Hello , world!

---
+++ data.js
@@ -0,0 +1,46 @@
+
+
+
+const Quiz_Obj = {
+    First_Question: {
+        text: '6^2 ?',
+        option: [6,12,36,72]
+    },
+    Second_Question: {
+        text: '2+2*2 ?',
+        option: [6,8,4,10]
+    },
+    Third_Question: {
+        text: '2^3 = 2*2*2 ?',
+        option: ['Yes','No']
+    },
+    Fourth_Question: {
+        text: '23 > 24 ?',
+        option: ['Yes','No']
+    },
+    Fiveth_Question: {
+        text: '6*6 ?',
+        option: [6,12,36,72]
+    },
+    Sixth_Question: {
+        text: '6*5 ?',
+        option: [20,30,60,90]
+    },
+    Seventh_Question: {
+        text: '5! ?',
+        option: [5,25,120,125]
+    },
+    Eighth_Question: {
+        text: '7+5 ?',
+        option: [12,13,15,35]
+    },
+    Nineth_Question: {
+        text: '6/0 ?',
+        option: [0,6,60,'Error']
+    },
+    Tenth_Question: {
+        text: '(10+10)*(10-5) ?',
+        option: [20,50,100,200]
+    },
+}
+
